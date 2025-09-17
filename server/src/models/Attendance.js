888af939b8ecc0bc9912239d8e298a5a65e5fce3
Amira <amirasyafikapohan@gmail.com>
2025-09-17 15:12:13 +0700

    menambahkan attendance.js

---
+++ server/src/models/Attendance.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const attendanceSchema = new mongoose.Schema({
+  studentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  status: {
+    type: String,
+    enum: ["present", "absent", "sick", "excused"], 
+    required: true
+  },
+  notes: {
+    type: String,
+    trim: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Attendance", attendanceSchema);
