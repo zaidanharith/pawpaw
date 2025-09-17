02efac047d14833aff32f01567e62d015b7b2218
agathaamalia <agathahusnaamalia2004@mail.ugm.ac.id>
2025-09-17 15:57:37 +0700

    edit student

--- server/src/models/Student.js
+++ server/src/models/Student.js
@@ -1,51 +1,38 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-    studentId: {
+     name: {
         type: String,
         required: true,
-        unique: true,
-        trim: true
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    parentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Parent'
-    },
-    parentPhone: {
-        type: String,
-        trim: true
-    },
-    name: {
-        type: String,
-        required: true,
-        trim: true
+        trim: true,
     },
     gender: {
         type: String,
         enum: ['Male', 'Female'],
-        required: true
+        required: true,
     },
     address: {
         type: String,
-        trim: true
-    },
-    email: {
-        type: String,
         trim: true,
-        lowercase: true
-    },  
+    },
+    birthDate : {
+        type: Date,
+        required: true,
+        trim: true,
+    }, 
     class: {
         type: String,
-        trim: true
+        trim: true,
     },
-    activity: [{
+    status : {
+        type: Boolean,
+        required: true,
+    },
+    attendance : {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Activity'
-    }]
+        ref: 'Attendance',
+        required: true,
+    },
 });
 
 module.exports = mongoose.model('Student', studentSchema);
