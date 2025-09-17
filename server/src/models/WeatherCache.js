4f9e08ee47de6e158e82ded1790f95443a69f586
agathaamalia <agathahusnaamalia2004@mail.ugm.ac.id>
2025-09-17 14:09:10 +0700

    edit parent message

---
+++ server/src/models/WeatherCache.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const parentMessageSchema = new mongoose.Schema({
+    parent: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Parent',
+        required: true
+    },
+    teacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    reply: {
+        type: String,
+        trim: true
+    },
+    sentAt: {
+        type: Date,
+        default: Date.now
+    },
+    readByTeacher: {
+        type: Boolean,
+        default: false
+    },
+    readByParent: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('ParentMessage', parentMessageSchema);
