0188bae9e5bb73ac9e708d87b0437cfcb4bb46b9
Zaidan Harith <myharithzaidan@gmail.com>
2025-09-17 13:49:16 +0700

    FIX: buat agar bisa data masuk ke mongodb

---
+++ server/src/models/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Activity: require('./Activity'),
+    Announcement: require('./Announcement'),
+    LiveReport: require('./LiveReport'),
+    Parent: require('./Parent'),
+    ParentMessage: require('./ParentMessage'),
+    Student: require('./Student'),
+    Teacher: require('./Teacher'),
+    User: require('./User'),
+    WeatherCache: require('./WeatherCache'),
+};
