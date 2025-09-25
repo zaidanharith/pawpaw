1438b9b4a2020255c06c849818125b1d04dc1ac4
Amira <amirasyafikapohan@gmail.com>
2025-09-25 16:05:32 +0700

    FEAT: mengedit fitur post attendance, mengedit announcement route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,8 +5,14 @@ const authRoutes = require('./auth.route');
 const weatherRoutes = require('./weather.route');
 const announcementRoutes = require('./announcement.route');
 
+
+const attendanceRoutes = require('./attendance.route');
+
 router.use('/', authRoutes);
 router.use('/', weatherRoutes);
 router.use('/', announcementRoutes);
+
+
+router.use('/', attendanceRoutes);
 
 module.exports = router;
